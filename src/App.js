99d0f099a03e157a5894986b8fe9c12d3e025c70
Sukrutha <sukrutha.areekat@citrusinformatics.com>
2022-03-30 16:19:11 -0700

    Active and Completed Cards seperated

--- src/App.js
+++ src/App.js
@@ -10,20 +10,27 @@ import {
   IconButton,
   CardActions,
   List,
-  Checkbox
+  Checkbox,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
 
 function App() {
   const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([0]);
+  const [todoList, setTodoList] = useState([]);
   const [date, setDate] = useState("");
 
-  const addTodo = () => {
+  const addTodo = (e) => {
+    e.preventDefault();
     setTodoList([
       ...todoList,
-      { id: Date.now(), text: todo, status: "true", date: date, completed: "false"  },
+      {
+        id: Date.now(),
+        text: todo,
+        status: "true",
+        date: date,
+        completed: "false",
+      },
     ]);
   };
 
@@ -34,76 +41,135 @@ function App() {
 
   return (
     <div className="App">
-      <Paper className="todo-container" elevation={15}>
-        <div className="text-input">
-          <TextField
-            className="text-field"
-            id="outlined-basic"
-            label="Add Todo"
-            variant="outlined"
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <Fab
-            color="primary"
-            aria-label="add"
-            onClick={() => {
-              addTodo();
-            }}
-          >
-            <AddIcon />
-          </Fab>
-        </div>
-        <div className="todo-list">
-          <List>
-            {todoList.map((item) => {
-              if (item.status === "true" && item.completed === "false") {
-                return (
-                  <Card className="todo-item" sx={{ minWidth: 275 }}>
-                    <CardActions>
-                    <Checkbox onChange={(e)=>{
-                      setTodoList(
-                        todoList.filter((obj)=>{
-                          if (obj.id===item.id){
-                            obj.completed="true"
-                          }
-                          return obj
-                        })
-                      )
-                    }} />
-                    </CardActions>
-                    <CardContent className="todo-content">
-                      <Typography variant="h5" component="div">
-                        {item.text}
-                      </Typography>
-                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        {item.date}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <IconButton
-                        aria-label="delete"
-                        onClick={(e) => {
-                          setTodoList(
-                            todoList.filter((obj) => {
-                              if (obj.id === item.id) {
-                                obj.status = "false";
-                              }
-                              return obj;
-                            })
-                          );
-                        }}
-                      >
-                        <DeleteOutlined color="primary" />
-                      </IconButton>
-                    </CardActions>
-                  </Card>
-                );
-              }
-              return null;
-            })}
-          </List>
-        </div>
-      </Paper>
+      <div className="App-container">
+        <Paper className="todo-container" elevation={15}>
+          <form className="text-input" onSubmit={addTodo}>
+            <TextField
+              required
+              className="text-field"
+              id="outlined-basic"
+              label="Add Todo"
+              variant="outlined"
+              onChange={(e) => setTodo(e.target.value)}
+            />
+            <Fab type="submit" color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </form>
+          <div className="todo-list">
+            <h2>Active Task</h2>
+            <List>
+              {todoList.map((item) => {
+                if (item) {
+                  if (item.status === "true" && item.completed === "false") {
+                    return (
+                      <Card className="todo-item" sx={{ minWidth: 275 }}>
+                        <CardActions>
+                          <Checkbox
+                            onChange={(e) => {
+                              setTodoList(
+                                todoList.filter((obj) => {
+                                  if (obj.id === item.id) {
+                                    obj.completed = "true";
+                                  }
+                                  return obj;
+                                })
+                              );
+                            }}
+                          />
+                        </CardActions>
+                        <CardContent className="todo-content">
+                          <Typography variant="h5" component="div">
+                            {item.text}
+                          </Typography>
+                          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            {item.date}
+                          </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <IconButton
+                            aria-label="delete"
+                            onClick={(e) => {
+                              setTodoList(
+                                todoList.filter((obj) => {
+                                  if (obj.id === item.id) {
+                                    obj.status = "false";
+                                  }
+                                  return obj;
+                                })
+                              );
+                            }}
+                          >
+                            <DeleteOutlined color="primary" />
+                          </IconButton>
+                        </CardActions>
+                      </Card>
+                    );
+                  }
+                  
+                }
+                return null;
+              })}
+            </List>
+          </div>
+        </Paper>
+        <Paper className="todo-container" elevation={15}>
+          <div className="todo-list">
+            <List>
+              <h2>Completed Task</h2>
+              {todoList.map((item) => {
+                if (item.completed === "true" && item.status === "true") {
+                  return (
+                    <Card className="todo-item" sx={{ minWidth: 275 }}>
+                      <CardActions>
+                        <Checkbox
+                          defaultChecked
+                          onChange={(e) => {
+                            setTodoList(
+                              todoList.filter((obj) => {
+                                if (obj.id === item.id) {
+                                  obj.completed = "false";
+                                }
+                                return obj;
+                              })
+                            );
+                          }}
+                        />
+                      </CardActions>
+                      <CardContent className="todo-content">
+                        <Typography variant="h5" component="div">
+                          {item.text}
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                          {item.date}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        <IconButton
+                          aria-label="delete"
+                          onClick={(e) => {
+                            setTodoList(
+                              todoList.filter((obj) => {
+                                if (obj.id === item.id) {
+                                  obj.status = "false";
+                                }
+                                return obj;
+                              })
+                            );
+                          }}
+                        >
+                          <DeleteOutlined color="primary" />
+                        </IconButton>
+                      </CardActions>
+                    </Card>
+                  );
+                }
+                return null;
+              })}
+            </List>
+          </div>
+        </Paper>
+      </div>
     </div>
   );
 }

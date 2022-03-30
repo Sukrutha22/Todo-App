c0e15d44d3984887844f4c13f95dca53c13e8246
Sukrutha <sukrutha.areekat@citrusinformatics.com>
2022-03-29 20:12:09 -0700

    Background margin padding resize

--- src/App.js
+++ src/App.js
@@ -1,71 +1,73 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-const [todo, setTodo] = useState('')
-const [todoList, setTodoList] = useState([0])
-const [date, setDate] = useState('')
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([0]);
+  const [date, setDate] = useState("");
 
-const addTodo = () =>{
-  setTodoList([...todoList, {id: Date.now(), text: todo, status: 'true'}])
-}
+  const addTodo = () => {
+    setTodoList([...todoList, { id: Date.now(), text: todo, status: "true" }]);
+  };
 
- 
-
-useEffect(()=>{
-  const today = new Date()
-  setDate(today.toDateString())
-},[todo])
-
+  useEffect(() => {
+    const today = new Date()
+    setDate(today.toDateString());
+  }, [todo]);
 
   return (
     <div className="App">
       <div className="todo-container">
         <div className="text-input">
-          <input onChange={(e) => setTodo(e.target.value)} type="text" placeholder='Add todo'/>
-          <button onClick={()=>{addTodo()}}>
-            <i className='fa fa-plus'></i>
+          <input
+            onChange={(e) => setTodo(e.target.value)}
+            type="text"
+            placeholder="Add todo"
+          />
+          <button
+            onClick={() => {
+              addTodo();
+            }}
+          >
+            <i className="fa fa-plus"></i>
           </button>
         </div>
-        <div  className="todo-list">
+        <div className="todo-list">
           <ul>
-
-            {todoList.map((item)=>{
-              if (item.status==='true'){
-                return(
+            {todoList.map((item) => {
+              if (item.status === "true") {
+                return (
                   <li>
-                  <div className="todo-item">
-                    <div className='todo-content'>
-                    <p className='todo-text'><b>{item.text}</b>
-                    </p>
-                    <p className='todo-date'>{date}</p>
-                    </div>
-                   
-                    <i className='fa fa-minus	' 
-                      onClick={(e) =>{
-                        item.status='false'
-                        setTodoList(todoList.filter(obj=>{
-                          if (obj.id === item.id){
-                            obj.status='false'
-                          }
-                          return obj;
-                        }))
+                    <div className="todo-item">
+                      <div className="todo-content">
+                        <p className="todo-text">
+                          <b>{item.text}</b>
+                        </p>
+                        <p className="todo-date">{date}</p>
+                      </div>
 
-                      }}
-                      value={item.status}
-                    ></i>
-                  </div>
-                </li>
-                )
+                      <i
+                        className="fa fa-minus	"
+                        onClick={(e) => {
+                          setTodoList(
+                            todoList.filter((obj) => {
+                              if (obj.id === item.id) {
+                                obj.status = "false";
+                              }
+                              return obj;
+                            })
+                          );
+                        }}
+                      ></i>
+                    </div>
+                  </li>
+                );
               }
-              return null
+              return null;
             })}
-           
           </ul>
         </div>
       </div>
-
     </div>
   );
 }

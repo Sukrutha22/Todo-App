6bc45c5ad2c23db84b9831cc2143dff35459d9ed
Sukrutha <sukrutha.areekat@citrusinformatics.com>
2022-03-31 15:54:14 -0700

    Login authentication

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,39 +1,69 @@
-import { Paper,Typography ,TextField,Button } from '@mui/material'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Paper, Typography, TextField, Button } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Firebase from "../firebaseConfig";
 
 function Login() {
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const submitLogin = (e) => {
+    e.preventDefault();
+    Firebase.auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        navigate("home");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div className="App-container">
-        
-      <Paper sx={{ height: "auto", width: 3/10 ,}} className="todo-container" elevation={15}>
-        <Typography variant="h4" sx={{ my: 2, color: 'primary.main'}}>Login</Typography>
+      <Paper
+        sx={{ height: "auto", width: 3 / 10 }}
+        className="todo-container"
+        elevation={15}
+      >
+        <Typography variant="h4" sx={{ my: 2, color: "primary.main" }}>
+          Login
+        </Typography>
         <form action="" className="login-form">
-        
           <TextField
-            required fullWidth
+            required
+            fullWidth
             id="outlined-required"
             label="Email Address"
             type="email"
-            sx={{mb: 2}}
+            sx={{ mb: 2 }}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          
+
           <TextField
-            required fullWidth
+            required
+            fullWidth
             id="outlined-password-input"
             label="Password"
             type="password"
             autoComplete="current-password"
-            sx={{mb: 2}}
+            sx={{ mb: 2 }}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <Button variant="contained" type="submit" sx={{width: 1/2}} onClick={()=>navigate('home')}>LOGIN</Button>
-          <Button variant="text" onClick={()=>navigate('signup')}>Sign Up</Button>
-
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ width: 1 / 2 }}
+            onClick={submitLogin}
+          >
+            LOGIN
+          </Button>
+          <Button variant="text" onClick={() => navigate("signup")}>
+            Sign Up
+          </Button>
         </form>
       </Paper>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;

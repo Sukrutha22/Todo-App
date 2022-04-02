6b67212d2e991c21fe36107618d1f7b617f84541
Sukrutha <sukrutha.areekat@citrusinformatics.com>
2022-04-01 20:29:24 -0700

    Assignee dropdown added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,10 @@ import {
   CardActions,
   List,
   Checkbox,
-  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteOutlined from "@mui/icons-material/DeleteOutlined";
@@ -22,11 +25,15 @@ function Home() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
   const [date, setDate] = useState("");
+  const [assigneeId, setAssigneeId] = useState("");
+  const [assigneeList, setAssigneeList] = useState([]);
   const today = new Date();
   const todos = Firebase.firestore().collection("todos").get();
+  const assignees = Firebase.firestore().collection("users").get();
 
   const addTodo = (e) => {
     e.preventDefault();
+    setDate(today.toDateString());
     Firebase.auth().onAuthStateChanged((user) => {
       Firebase.firestore().collection("todos").add({
         text: todo,
@@ -37,160 +44,183 @@ function Home() {
     });
   };
 
-  useEffect(() => {
-    setDate(today.toDateString());
-    Firebase.auth().onAuthStateChanged((user) => {
-      Firebase.firestore()
-        .collection("todos")
-        .get()
-        .then((snapshot) => {
-          const allTodo = snapshot.docs.map((item) => {
-            console.log(item.data());
-            return (
-              item.data().userId === user.uid && { ...item.data(), id: item.id }
-            );
-          });
+  const handleAssignee = (e) => {
+    e.preventDefault();
+    setAssigneeId(e.target.value);
+  };
 
-          setTodoList(allTodo);
+  useEffect(() => {
+    Firebase.auth().onAuthStateChanged((user) => {
+      Firebase.firestore().collection("todos").get().then((snapshot) => {
+        const allTodo = snapshot.docs.map((item) => {
+          return (
+            item.data().userId === user.uid && { ...item.data(), id: item.id }
+          );
         });
+
+        setTodoList(allTodo);
+      });
     });
-  }, [todos]);
+    Firebase.firestore().collection("users").get().then((snapshot) => {
+      const allAssignee = snapshot.docs.map((item) => {
+        return { ...item.data() };
+      });
+      setAssigneeList(allAssignee);
+    });
+  }, [todos, assignees]);
 
   return (
     <div className="App-container">
-      <Paper className="todo-container" elevation={15}>
-        <form className="text-input" onSubmit={addTodo}>
-          <TextField
-            required
-            className="text-field"
-            id="outlined-basic"
-            label="Add Todo"
-            variant="outlined"
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <Fab type="submit" color="primary" aria-label="add">
-            <AddIcon />
-          </Fab>
-        </form>
-        <div className="todo-list">
-          <h2>Active Task</h2>
-          <List>
-            <Typography
-              variant="h6"
-              className={todoList.length ? "display-none" : "display"}
-            >
-              No Task Yet
-            </Typography>
-            {todoList.map((item) => {
-              console.log(item.id);
-              if (item.completed === "false") {
-                return (
-                  <Card className="todo-item" sx={{ minWidth: 275 }}>
-                    <CardActions>
-                      <Checkbox
-                        onChange={(e) => {
-                          Firebase.firestore()
-                            .collection("todos")
-                            .doc(item.id)
-                            .update({
-                              completed: "true",
-                            });
-                        }}
-                      />
-                    </CardActions>
-                    <CardContent className="todo-content">
-                      <Typography variant="subtitle1" component="div">
-                        {item.text}
-                      </Typography>
-                      <Typography
-                        variant="caption"
-                        sx={{ mb: 1.5 }}
-                        color="text.secondary"
-                      >
-                        {item.date}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <IconButton
-                        aria-label="delete"
-                        onClick={(e) => {
-                          Firebase.firestore()
-                            .collection("todos")
-                            .doc(item.id)
-                            .delete();
-                        }}
-                      >
-                        <DeleteOutlined color="primary" />
-                      </IconButton>
-                    </CardActions>
-                  </Card>
-                );
-              }
+      <div className="ticket-container">
+        <Paper elevation={10} sx={{ width: 1 / 2 }}>
+          <form className="text-input" onSubmit={addTodo}>
+            <TextField
+              required
+              id="outlined-basic"
+              label="Add Ticket"
+              variant="outlined"
+              sx={{ width: 4.5 / 10 }}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+            <FormControl sx={{ width: 4.5 / 10 }}>
+              <InputLabel id="demo-simple-select-label">Assignee</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={assigneeId}
+                label="Assigned To"
+                onChange={handleAssignee}
+              >
+                {assigneeList.map((item) => {
+                  if (item.role === "Engineer") {
+                    return <MenuItem value={item.id}>{item.username}</MenuItem>;
+                  }
+                  return null;
+                })}
+              </Select>
+            </FormControl>
+            <Fab type="submit" color="primary" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </form>
+        </Paper>
+      </div>
 
-              return null;
-            })}
-          </List>
-        </div>
+      <Paper className="todo-container" elevation={15}>
+        <h2>Active Task</h2>
+        <List>
+          <Typography
+            variant="h6"
+            className={todoList.length ? "display-none" : "display"}
+          >
+            No Task Yet
+          </Typography>
+          {todoList.map((item) => {
+            if (item.completed === "false") {
+              return (
+                <Card className="todo-item" sx={{ minWidth: 275 }}>
+                  <CardActions>
+                    <Checkbox
+                      onChange={(e) => {
+                        Firebase.firestore()
+                          .collection("todos")
+                          .doc(item.id)
+                          .update({
+                            completed: "true",
+                          });
+                      }}
+                    />
+                  </CardActions>
+                  <CardContent className="todo-content">
+                    <Typography variant="subtitle1" component="div">
+                      {item.text}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      sx={{ mb: 1.5 }}
+                      color="text.secondary"
+                    >
+                      {item.date}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={(e) => {
+                        Firebase.firestore()
+                          .collection("todos")
+                          .doc(item.id)
+                          .delete();
+                      }}
+                    >
+                      <DeleteOutlined color="primary" />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              );
+            }
+
+            return null;
+          })}
+        </List>
       </Paper>
       <Paper className="todo-container " elevation={15}>
-        <div className="todo-list">
-          <List>
-            <h2>Completed Task</h2>
-            <Typography
-              variant="h6"
-              className={todoList.length ? "display-none" : "display"}
-            >
-              No Completed Task
-            </Typography>
-            {todoList.map((item) => {
-              if (item.completed === "true") {
-                return (
-                  <Card className="todo-item" sx={{ minWidth: 275 }}>
-                    <CardActions>
-                      <Checkbox
-                        defaultChecked
-                        onChange={(e) => {
-                          Firebase.firestore()
-                            .collection("todos")
-                            .doc(item.id)
-                            .update({
-                              completed: "false",
-                            });
-                        }}
-                      />
-                    </CardActions>
-                    <CardContent className="todo-content">
-                      <Typography variant="subtitle1" component="div">
-                        {item.text}
-                      </Typography>
-                      <Typography
-                        variant="caption"
-                        sx={{ mb: 1.5 }}
-                        color="text.secondary"
-                      >
-                        {item.date}
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <IconButton
-                        aria-label="delete"
-                        onClick={(e) => {
-                          Firebase.firestore()
-                            .collection("todos")
-                            .doc(item.id)
-                            .delete();
-                        }}
-                      >
-                        <DeleteOutlined color="primary" />
-                      </IconButton>
-                    </CardActions>
-                  </Card>
-                );
-              }
-              return null;
-            })}
-          </List>
-        </div>
+        <h2>Completed Task</h2>
+        <List>
+          <Typography
+            variant="h6"
+            className={todoList.length ? "display-none" : "display"}
+          >
+            No Completed Task
+          </Typography>
+          {todoList.map((item) => {
+            if (item.completed === "true") {
+              return (
+                <Card className="todo-item" sx={{ minWidth: 275 }}>
+                  <CardActions>
+                    <Checkbox
+                      defaultChecked
+                      onChange={(e) => {
+                        Firebase.firestore()
+                          .collection("todos")
+                          .doc(item.id)
+                          .update({
+                            completed: "false",
+                          });
+                      }}
+                    />
+                  </CardActions>
+                  <CardContent className="todo-content">
+                    <Typography variant="subtitle1" component="div">
+                      {item.text}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      sx={{ mb: 1.5 }}
+                      color="text.secondary"
+                    >
+                      {item.date}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={(e) => {
+                        Firebase.firestore()
+                          .collection("todos")
+                          .doc(item.id)
+                          .delete();
+                      }}
+                    >
+                      <DeleteOutlined color="primary" />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              );
+            }
+            return null;
+          })}
+        </List>
       </Paper>
     </div>
   );
